44335c832690c569ba89aede111a47f7fd382c8f
Lucas Comamala <lucascomamala@gmail.com>
2022-09-30 17:50:08 -0600

    Fixed render method

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-import './index.css';
 import store from './redux/configureStore';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
