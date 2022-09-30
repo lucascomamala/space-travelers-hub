4d10bedcd6db48acc54032c76260b3f680134016
Lucas Comamala <lucascomamala@gmail.com>
2022-09-30 17:36:48 -0600

    FIxed rockets page

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducerMissions from './missionsSlice';
+import rockets from './rocketsSlice';
 
 const store = configureStore({
   reducer: {
+    rockets,
     missions: reducerMissions,
   },
   middleware: [thunk, logger],
